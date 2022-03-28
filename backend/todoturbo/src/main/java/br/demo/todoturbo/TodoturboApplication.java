e68f1c5d58e9b9e93995242a899803576e2b1c2d
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-03-28 00:28:29 -0300

    feat: base spring initializr project

---
+++ backend/todoturbo/src/main/java/br/demo/todoturbo/TodoturboApplication.java
@@ -0,0 +1,13 @@
+package br.demo.todoturbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoturboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoturboApplication.class, args);
+	}
+
+}
---
+++ backend/todoturbo/src/test/java/br/demo/todoturbo/TodoturboApplicationTests.java
@@ -0,0 +1,13 @@
+package br.demo.todoturbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoturboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
