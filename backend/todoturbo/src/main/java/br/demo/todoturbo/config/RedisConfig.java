4c6bb5a7d997c22da6cf2eb1bcadd201520c1362
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-04-01 01:54:46 -0300

    feat: conexão do backend com o Redis
    Referências:
    
    https://stackoverflow.com/questions/61701003/how-connection-pooling-works-with-redistemplate-using-spring-boot-application
    
    https://stackoverflow.com/questions/30447919/cannot-get-connection-for-redistemplate-for-spring-data-redis/31023359#31023359

---
+++ backend/todoturbo/src/main/java/br/demo/todoturbo/config/RedisConfig.java
@@ -0,0 +1,47 @@
+package br.demo.todoturbo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${todoTurbo.db.hostname}")
+    private String redisHostName;
+
+    @Value("${todoTurbo.db.port}")
+    private int redisPort;
+
+    @Bean
+    public JedisClientConfiguration getClientConfig() {
+        JedisClientConfiguration.JedisClientConfigurationBuilder builder =
+                JedisClientConfiguration.builder();
+
+        var connectionTimeout = Duration.ofSeconds(30);
+
+        return builder.connectTimeout(connectionTimeout)
+                .usePooling().build();
+    }
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        var environmentalConfig = new RedisStandaloneConfiguration(redisHostName, redisPort);
+
+        return new JedisConnectionFactory(environmentalConfig, getClientConfig());
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+
+}
