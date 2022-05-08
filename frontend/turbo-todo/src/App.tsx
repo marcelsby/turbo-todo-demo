90a5e541d5099eaea6b7fc1adc3b7ff382dfa2a0
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-05-08 14:09:00 -0300

    feat(deps): tailwind & phosphor-icons

--- frontend/turbo-todo/src/App.tsx
+++ frontend/turbo-todo/src/App.tsx
@@ -1,3 +1,10 @@
+import { Trash } from 'phosphor-react'
+
 export function App() {
-    return <h1>Hello World</h1>
+    return (
+        <h1 className="text-red-600">
+            <Trash size={64} />
+            Hello World
+        </h1>
+    )
 }
